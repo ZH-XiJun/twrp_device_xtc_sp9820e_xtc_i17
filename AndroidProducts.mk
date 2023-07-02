e31661dd7c61f72ba137255fae4555d652da99c5
zh-xijun <59949135+ZH-XiJun@users.noreply.github.com>
2023-07-02 09:56:05 +0800

    Initial

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_sp9820e_xtc.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_sp9820e_xtc-user \
+    omni_sp9820e_xtc-userdebug \
+    omni_sp9820e_xtc-eng
---
+++ README.md
@@ -0,0 +1,10 @@
+# Android device tree for XTC XTC Z1S (sp9820e_xtc)
+
+```
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
